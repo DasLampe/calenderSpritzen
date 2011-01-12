3efd0c13109564b64aa7dee00ab2dab795eb6b68
DasLampe <daslampe@lano-crew.org>
2011-01-12 12:43:32 +0100

    Clean Repo

--- tmp/_css.php
+++
@@ -1,7 +0,0 @@
-<?php
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2011 DasLampe <dasLampe@lano-crew.org> |
-// | Encoding:  UTF-8 |
-// +----------------------------------------------------------------------+
-?>
-	<link rel="stylesheet" type="text/css" href="http://localhost//calenderSpritzen/css/style.css" />
