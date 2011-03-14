ac7d0e39bc8433e307c1ed7f175576906742271a
DasLampe <daslampe@lano-crew.org>
2011-03-14 13:59:43 +0100

    Fix bug in bash.info.php

--- bash.info.php
+++ bash.info.php
@@ -1,5 +1,5 @@
 <?php
-include_once(dirname(__FILE__)."lib/class/spritzen.class.php");
+include_once(dirname(__FILE__)."/lib/class/spritzen.class.php");
 $spritzen	= new spritzen(dirname(__FILE__)."/stat.xml");
 
 if($spritzen->getNotification() === true)
